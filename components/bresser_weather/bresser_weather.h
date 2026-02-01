fc4a80611918a10d4e2993bb5cf7edf00ee7a015
magicflofla <122453938+magicflofla@users.noreply.github.com>
2026-02-01 23:16:21 +0100

    Update bresser_weather.h

--- components/bresser_weather/bresser_weather.h
+++ components/bresser_weather/bresser_weather.h
@@ -1,11 +1,16 @@
 #pragma once
 
+/*
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "WeatherSensorCfg.h"
 #include "WeatherSensor.h"
+*/
+#include "esphome.h"
+#include "RadioLib.h"
+#include "esphome/core/gpio.h"
 
 #if defined(ESP32)
     #define PIN_CC1101_CS   5
